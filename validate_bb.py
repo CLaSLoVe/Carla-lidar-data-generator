c2069a30a365e9c6e95c526503f570b66df959c6
CLaSLoVe <jindian_0805@foxmail.com>
2022-07-08 16:06:22 +0800

    bug fixed

--- validate_bb.py
+++ validate_bb.py
@@ -120,30 +120,30 @@ def validate(posit_path, points_path, pic_path, label_path):
 #                   ROOT+'lidar\\'+NAME+'.bin',
 #                   ROOT+'pics\\'+NAME+'.png',
 #                   ROOT+'label\\'+NAME+'.txt',)
-
-
-def batch_validate(PATH):
-    for i in [0.4, 0.8, 1.6]:
-        ROOT = PATH+'\\'+str(i)+'\\'
-        path = PATH+'\\'+str(i)+'\\lidar'
-        frames = [x.split('.')[0] for x in os.listdir(path)]
-        for f, frame in enumerate(tqdm(frames)):
-            labels = validate(ROOT+'posit\\'+frame+'.pkl',
-                              ROOT+'lidar\\'+frame+'.bin',
-                              ROOT+'pics\\'+frame+'.png',
-                              ROOT+'label\\'+frame+'.txt',)
-            # print(i, ':', f, ':', frame)
-    return PATH
-
-
-if __name__ == '__main__':
-    # PATH = 'D:\jd\WindowsNoEditor\PythonAPI\my\data\\2022-07-06 17.35.28'
-    ROOT = 'D:\jd\WindowsNoEditor\PythonAPI\my\data\\2022-07-07 10.19.16\\0.8\\'
-    # batch_validate(PATH)
-    NAME = '0000040962'
-    labels = validate(ROOT+'posit\\'+NAME+'.pkl',
-                      ROOT+'lidar\\'+NAME+'.bin',
-                      ROOT+'pics\\'+NAME+'.png',
-                      ROOT+'label\\'+NAME+'.txt',)
+#
+#
+# def batch_validate(PATH):
+#     for i in [0.4, 0.8, 1.6]:
+#         ROOT = PATH+'\\'+str(i)+'\\'
+#         path = PATH+'\\'+str(i)+'\\lidar'
+#         frames = [x.split('.')[0] for x in os.listdir(path)]
+#         for f, frame in enumerate(tqdm(frames)):
+#             labels = validate(ROOT+'posit\\'+frame+'.pkl',
+#                               ROOT+'lidar\\'+frame+'.bin',
+#                               ROOT+'pics\\'+frame+'.png',
+#                               ROOT+'label\\'+frame+'.txt',)
+#             # print(i, ':', f, ':', frame)
+#     return PATH
+#
+#
+# if __name__ == '__main__':
+#     # PATH = 'D:\jd\WindowsNoEditor\PythonAPI\my\data\\2022-07-06 17.35.28'
+#     ROOT = 'D:\jd\WindowsNoEditor\PythonAPI\my\data\\2022-07-07 10.19.16\\0.8\\'
+#     # batch_validate(PATH)
+#     NAME = '0000040962'
+#     labels = validate(ROOT+'posit\\'+NAME+'.pkl',
+#                       ROOT+'lidar\\'+NAME+'.bin',
+#                       ROOT+'pics\\'+NAME+'.png',
+#                       ROOT+'label\\'+NAME+'.txt',)
 
 
